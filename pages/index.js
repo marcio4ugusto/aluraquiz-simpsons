a9820ae24404812db52f1ccd5e1b47fd96d624fc
Marcio Bernardes <marcim.bernardes@gmail.com>
2021-01-25 18:02:53 -0300

    feat: add SEO metatags

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import Head from 'next/head';
+
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -12,6 +15,8 @@ import GitHubCorner from '../src/components/GitHubCorner'
 //   background-size: cover;
 //   background-position: center;
 // `;
+
+const currentURL = ''
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -26,28 +31,41 @@ export const QuizContainer = styled.div`
 
 export default function Home() {
   return (
-    <QuizBackground backgroundImage={db.bg}>
-      <QuizContainer>
-        <QuizLogo />
-        <Widget>
-          <Widget.Header>
-            <h1>{db.title}</h1>
-          </Widget.Header>
-          <Widget.Content>
-            <p>{db.description}</p>
-          </Widget.Content>
-        </Widget>
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content={db.description} />
+        <title>{db.title}</title>
 
-        <Widget>
-          <Widget.Content>
-            <h1>Quizes da Galera</h1>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={currentURL} key="ogurl" />
+        <meta property="og:title" content={db.title} key="title" />
+        <meta property="og:description" content={db.description} key="ogdesc" />
+        <meta property="og:image" content={db.bg} key="ogimage" />
+      </Head>
+      <QuizBackground backgroundImage={db.bg}>
+        <QuizContainer>
+          <QuizLogo />
+          <Widget>
+            <Widget.Header>
+              <h1>{db.title}</h1>
+            </Widget.Header>
+            <Widget.Content>
+              <p>{db.description}</p>
+            </Widget.Content>
+          </Widget>
 
-            <p>lorem ipsum dolor sit amet...</p>
-          </Widget.Content>
-        </Widget>
-        <Footer />
-      </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/marcio-bernardes/aluraquiz-vacinacao" />
-    </QuizBackground>
+          <Widget>
+            <Widget.Content>
+              <h1>Quizes da Galera</h1>
+
+              <p>lorem ipsum dolor sit amet...</p>
+            </Widget.Content>
+          </Widget>
+          <Footer />
+        </QuizContainer>
+        <GitHubCorner projectUrl="https://github.com/marcio-bernardes/aluraquiz-vacinacao" />
+      </QuizBackground>
+    </>
   );
 }
