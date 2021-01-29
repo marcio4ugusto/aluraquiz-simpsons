e08103e506a7d0ec6c42fa143510eb842b72c122
Marcio Bernardes <marcim.bernardes@gmail.com>
2021-01-29 11:23:20 -0300

    style: change background image

--- src/components/QuizBackground/index.js
+++ src/components/QuizBackground/index.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const QuizBackground = styled.div`
   width: 100%;
   background-size: cover;
-  background-position: center;
+  background-position: 220px top;
   background-image: url(${({ backgroundImage }) => backgroundImage});
   background-color: ${({ theme }) => theme.colors.mainBg};
   flex: 1;
